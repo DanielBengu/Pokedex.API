d6e02abe795b5c7a1a1130ee3ae24bed6e861bcd
DanielBengu <38065137+DanielBengu@users.noreply.github.com>
2024-09-18 23:56:11 +0200

    Aggiungere i file di progetto.

---
+++ Pokedex API Business/V1/PokedexApiService.cs
@@ -0,0 +1,42 @@
+﻿using static Pokedex.API.Model.V1.HttpRequestGet;
+
+namespace Pokedex.API.Business.V1
+{
+    public interface IPokedexApiService
+    {
+        GetBasicResponse GetBasicById(GetBasicRequest pokemon);
+        GetTranslatedResponse GetTranslatedById(GetTranslatedRequest pokemon);
+    }
+
+    public class PokedexApiService : IPokedexApiService
+    {
+        public GetBasicResponse GetBasicById(GetBasicRequest request)
+        {
+            return new()
+            {
+                Pokemon = new()
+                {
+                    Name = request.Pokemon,
+                    Description = request.Pokemon,
+                    Habitat = request.Pokemon,
+                    IsLegendary = true
+                }
+            };
+        }
+
+        public GetTranslatedResponse GetTranslatedById(GetTranslatedRequest request)
+        {
+            return new()
+            {
+                Pokemon = new()
+                {
+                    Name = request.Pokemon,
+                    Description = request.Pokemon,
+                    Habitat = request.Pokemon,
+                    IsLegendary = true
+                }
+            };
+        }
+
+    }
+}
---
+++ Pokedex API/Controllers/PokemonController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.API.Business.V1;
+using Pokedex.API.Model.V1;
+using static Pokedex.API.Model.V1.HttpRequestGet;
+
+namespace Pokedex.API.Controllers.V1
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController(ILogger<PokemonController> logger, IPokedexApiService apiService) : ControllerBase
+    {
+        private readonly ILogger<PokemonController> _logger = logger;
+        private readonly IPokedexApiService _apiService = apiService;
+
+        [HttpGet]
+        public GetBasicResponse GetBasicById([FromBody]GetBasicRequest pokemon)
+        {
+            _logger.LogInformation("GetBasicById request for Pokemon: {pokemon}", pokemon);
+            return _apiService.GetBasicById(pokemon);
+        }
+
+        [HttpGet("Translated")]
+        public GetTranslatedResponse GetTranslatedById([FromBody]GetTranslatedRequest pokemon)
+        {
+            _logger.LogInformation("GetTranslatedById request for Pokemon: {pokemon}", pokemon);
+            return _apiService.GetTranslatedById(pokemon);
+        }
+    }
+}
